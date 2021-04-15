1acb216a6ec09fb136acb06bf384fcc750543350
Andres Zapata <azapata@uc.edu.co>
2021-04-15 11:09:13 -0500

    Prueba de rama de tarea para indra

--- client.js
+++ client.js
@@ -7,18 +7,26 @@ const readline = require('readline').createInterface({
 
 
 const socket= new Socket();
-
+const END ='END';
 
 socket.connect({host: 'localhost',port:8000 });
 
 socket.write('Hola mundo desde sockets node');
 socket.setEncoding('utf-8');
 
-readline.on('line',(line)=>{
-    socket.write(line);
+readline.on('line',(message)=>{
+    if(message === END){
+        socket.end();
+        process.exit(0);
+
+    }
+    socket.write(message);
 })
 
 
 socket.on('data',(data)=>{
     console.log(data);
 })
+
+socket.on("close", ()=>
+process.exit(0));
--- server.js
+++ server.js
@@ -3,16 +3,34 @@ const { Server } = require("net");
 
 const server = new Server();
 
+//protocolo para cerrar Socket
+const END = 'END';
+
+
+// evento de la conexion al socket
 server.on("connection", (socket) => {
     const remoteSocket=`${socket.remoteAddress}: ${socket.remotePort}`;
   console.log(`New conection from ${remoteSocket}`);
 
   socket.setEncoding('utf-8');
+  // evento de cuando ingrese data a socket
   socket.on('data',(message)=>{
+    
+    if ( message === END){
+      
+      socket.end();
+    }else{
       console.log(`${remoteSocket} -> ${message}`);
-  })
+    }
+     
+  });
+  // evento de close de server
+  socket.on("close", ()=>console.log(`Conection wiht ${sockect.remotePort} has end`));
 });
 
 server.listen({ port: 8000, host: "0.0.0.0" }, () => {
   console.log("Listening on port 8000");
 });
+
+
+
